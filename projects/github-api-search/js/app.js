595f2ed0b9be40f445aeda745a9e48dfa4f4dbb1
Daniel Rodriguez <danielrodriguez@Daniels-MacBook-Pro.local>
2019-09-18 17:12:56 -0600

    Steven-Solutions

--- projects/github-api-search/js/app.js
+++ projects/github-api-search/js/app.js
@@ -1,7 +1,37 @@
+var arr = []
 var usersList = document.createElement('ul');
 
 function showUsers(){
     alert('show users');
+
+    //var info = document.getElementById('info');
+
+var xhttp = new XMLHttpRequest();
+
+xhttp.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+        arr = JSON.parse(this.responseText);
+
+        for(var i = 0; i < arr.length; i++){
+	        console.log(arr[i].avatar_url && arr[i].followers_url.length)
+	
+	        var userElement = document.createElement('li');
+	        var userFollowers = document.createElement ('p');
+	        var user = document.createTextNode(arr[i].login);
+	
+	        userElement.innerHTML = '<img src =' + arr[i].avatar_url + '/>';
+	
+            userFollowers.appendChild(user);
+            userElement.appendChild(userFollowers);
+            usersList.appendChild(userElement);
+            //info.appendChild(usersList);
+            document.getElementById('displayUsers').appendChild(usersList);
+        }
+    }
+};
+
+xhttp.open("GET", "https://api.github.com/users", true);
+xhttp.send();
  /* 
     1) You need to loop through array and then extract user picture
         Example: arr[i].avatar_url
@@ -24,6 +54,26 @@ function showUsers(){
 
 function beginSearch(){
     alert('begin search');
+
+    var userInput = document.getElementById("search").value
+
+    for(var i = 0; i < arr.length; i++){
+        if(arr[i].login === userInput){
+            usersList.innerHTML = ' ';
+            var user = document.createElement('li');
+            var userFollowers = document.createElement('p');
+            user.innerHTML = '<img src' + arr[i].avatar_url + '/>';
+    
+           user.appendChild(userFollowers)
+
+            return;
+        }
+               
+    }
+
+    alert('Users not found')
+
+    
     /*
     1) Get value entered from user in input box field. 
     2) Loop through your array to check if the value entered is an exisiting user in your array
